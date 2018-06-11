86b1f1cb5583711d810a03b0b0729655bb68311a
Mike Henderyckx <mike.henderyckx@gmail.com>
2018-06-11 21:14:53 +0200

    Add simple form with textarea & implement onTextareaChange + kickstart generateDocx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      cvJSON: "",
+    };
+  }
+
+  generateDocx = (e) => {
+    e.preventDefault();
+    console.log('THIS IS JSON', this.state.cvJSON);
+  };
+
+  onTextareaChange = (event) => {
+    this.setState({
+      cvJSON: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"><code>json</code> to <code>docx</code></h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Paste <code>json</code> here:.
         </p>
+        <form onSubmit={this.generateDocx} style={{ width: '480px', margin: '0 auto' }}>
+          <textarea
+            name="cv"
+            id="cv"
+            onChange={this.onTextareaChange}
+            style={{ width: '480px', height: '240px'}}
+          />
+          <button
+            style={{ display: 'block' }}
+            type="submit"
+          >
+            Generate .docx
+          </button>
+        </form>
       </div>
     );
   }
